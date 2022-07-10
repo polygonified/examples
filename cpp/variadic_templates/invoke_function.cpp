5f87c02f61db2ed335f515686fcda30f1f1dca99
polygonified <104267792+polygonified@users.noreply.github.com>
2022-07-10 15:05:06 -0400

    Create invoke_function.cpp

---
+++ cpp/variadic_templates/invoke_function.cpp
@@ -0,0 +1,9 @@
+// Takes in a function and calls it.
+// You can pass in arguments to the function after passing in the function.
+// Returns the output of the function called.
+ 
+template <typename T, typename... Args>
+void invoke(T (*f)(Args...))
+{
+    return (*f)(Args...);
+}
